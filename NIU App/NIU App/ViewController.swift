6465d47f870c7bf842e847f454c56907b840619e
ken6078 <ken60786213@gmail.com>
2023-05-09 22:35:43 +0800

    Feat: tensorflow lite model and example

--- NIU App/NIU App/ViewController.swift
+++ NIU App/NIU App/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import TensorFlowLiteTaskVision
 
 class ViewController: UIViewController {
     
@@ -14,6 +15,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        guard let modelPath = Bundle.main.path(
+            forResource: "model-meta",
+            ofType: "tflite"
+        ) else {
+            print("ModelPathError")
+            return
+        }
+        let options = ImageClassifierOptions(modelPath: modelPath)
+        
+        do {
+            let classifier = try ImageClassifier.classifier(options: options)
+            let image = UIImage(named: "test")!
+            let mlImage = MLImage(image: image)!
+            let classificationResults = try classifier.classify(mlImage: mlImage)
+            print("classificationResults:", classificationResults.classifications[0].categories[0].index)
+        } catch {
+            print("ERROR")
+            print(error)
+        }
+        
         
         testLabel.text = "Hello World"
         
@@ -21,6 +42,7 @@ class ViewController: UIViewController {
         testLabel.translatesAutoresizingMaskIntoConstraints = false
         testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         testLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor).isActive = true
+        
     }
 
 
